d678006a19a888694d11419073144acd8d5770cb
Nicolas Facchinetti <facchinetti.nicolas@outlook.it>
2021-03-16 17:49:22 +0100

    add pca and refactor notebooks

---
+++ transform.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class PCA:
+    def __init__(self, data):
+        self.data = data
+        cov_mat = np.cov(data, rowvar=0)                                # get covariance matrix
+        eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)          # get eigen values and eigen vectors 
+        
+        sorted_index = np.argsort(eigen_values)[::-1]                   # sort the index of eigenvalues in descending order
+        self.eigenvalues = eigen_values[sorted_index]                  
+        self.eigenvectors = eigen_vectors[:,sorted_index]
+
+    def get_principal_components(self, n_components):
+        eigenvector_subset = self.eigenvectors[:,0:n_components]
+        X_reduced = np.dot(eigenvector_subset.transpose(), self.data.transpose()).transpose()
+        return X_reduced
+     
+    def get_percentage_of_variance(self):
+        total_variance = np.sum(self.eigenvalues)
+
+        variances = []
+        for value in self.eigenvalues:
+            variances.append(value/total_variance)
+        return variances
